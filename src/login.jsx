1be7b87ad53b84e608bf7f8c3a6f9c03893d013a
Steven <stevendeschanel@outlook.com>
2022-11-05 20:14:50 -0500

    creating project

---
+++ src/login.jsx
@@ -0,0 +1,8 @@
+export const login = () => {
+    return (
+        <form>
+            <label for="email">email</label>
+            <input type="email" placeholder="uremail@gmail.com" name="email"/>
+        </form>
+    )
+}

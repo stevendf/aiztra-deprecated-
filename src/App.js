a5636d7e7734c43a76f670b7332afe2f1905e8a4
Steven <stevendeschanel@outlook.com>
2022-11-05 21:37:22 -0500

    creacion de formularios de ingreso y registro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, { useState } from "react";
 import './App.css';
-import { login } from "./login"
-import { register } from "./register"
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <login />
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
--- src/register.jsx
+++ src/register.jsx
@@ -1,5 +1,28 @@
-export const register = () => {
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return (
-        <> register </>
+        <div className="auth-form-container">
+            <h2>Registrarse</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Nombres</label>
+            <input value={name} name="name" id="name" placeholder="full Name" />
+            <label htmlFor="email">Correo</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">Contraseña</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <button type="submit">Registrarse</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>¿Ya tienes una cuenta? Ingresa aquí.</button>
+    </div>
     )
 }
